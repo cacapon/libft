43bf20a00a2dc0c4022056b70fc767f4a32436b9
Takuma Tsubo <ttsubo@c4r2s12.42tokyo.jp>
2024-10-28 13:01:15 +0900

    add strnstr

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -1,28 +1,37 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ttsubo <ttsubo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/10/28 12:30:54 by ttsubo            #+#    #+#             */
-/*   Updated: 2024/10/28 13:01:03 by ttsubo           ###   ########.fr       */
+/*   Created: 2024/10/28 12:44:31 by ttsubo            #+#    #+#             */
+/*   Updated: 2024/10/28 13:00:50 by ttsubo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
-	size_t	i;
+	size_t	h_i;
+	size_t	n_i;
 
-	i = 0;
-	while (src[i] && i < dstsize - 1)
+	if (*needle == '\0')
+		return ((char *)haystack);
+	h_i = 0;
+	while (haystack[h_i] && h_i < len)
 	{
-		dst[i] = src[i];
-		i++;
+		n_i = 0;
+		while (needle[n_i] && haystack[h_i + n_i] && (h_i + n_i) < len)
+		{
+			if (haystack[h_i + n_i] != needle[n_i])
+				break ;
+			n_i++;
+		}
+		if (needle[n_i] == '\0')
+			return ((char *)&haystack[h_i]);
+		h_i++;
 	}
-	if (dstsize > 0)
-		dst[i] = '\0';
-	return (ft_strlen(src));
+	return (NULL);
 }

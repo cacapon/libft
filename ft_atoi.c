f7487a741e2e55f8314c3ce7a56725d9045d7214
Takuma Tsubo <ttsubo@c4r2s12.42tokyo.jp>
2024-10-28 18:07:48 +0900

    ft_atoi;符号は一つしか受け付けないように修正

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: ttsubo <ttsubo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/28 14:46:32 by ttsubo            #+#    #+#             */
-/*   Updated: 2024/10/28 18:00:49 by ttsubo           ###   ########.fr       */
+/*   Updated: 2024/10/28 18:07:21 by ttsubo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,22 +25,20 @@ int	ft_atoi(const char *str)
 {
 	int		i;
 	int		sign;
-	int		has_sign;
 	long	num;
 
 	i = 0;
 	sign = 1;
-	has_sign = 0;
 	num = 0;
 	while (_isspace(str[i]))
 		i++;
-	while (str[i] == '-' || str[i] == '+')
-	{
-		has_sign = 1;
+	if (str[i] == '+')
 		i++;
-	}
-	if (has_sign && str[i - 1] == '-')
+	else if (str[i] == '-')
+	{
+		i++;
 		sign = -1;
+	}
 	while (_isdigit(str[i]))
 	{
 		num = num * 10 + (str[i] - '0');

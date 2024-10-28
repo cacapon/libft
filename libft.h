485f72fe006f06c32f8bdb7b24562bb25973714e
Takuma Tsubo <ttsubo@c4r2s12.42tokyo.jp>
2024-10-28 11:54:01 +0900

    add ft_bzero

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: ttsubo <ttsubo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/28 11:41:08 by ttsubo            #+#    #+#             */
-/*   Updated: 2024/10/28 11:42:00 by ttsubo           ###   ########.fr       */
+/*   Updated: 2024/10/28 11:53:36 by ttsubo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,13 +29,12 @@ int		ft_strlen(char *str);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 int		ft_tolower(int c);
 int		ft_toupper(int c);
-size_t	ft_strlcat(char *restrict dst, const char *restrict src,
-			size_t dstsize);
-size_t	ft_strlcpy(char *restrict dst, const char *restrict src,
-			size_t dstsize);
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
 void	*ft_calloc(size_t count, size_t size);
 void	*ft_memchr(const void *s, int c, size_t n);
 void	*ft_memmove(void *dst, const void *src, size_t len);
 void	*ft_memset(void *b, int c, size_t len);
+void	ft_bzero(void *b, size_t len);
 
 #endif

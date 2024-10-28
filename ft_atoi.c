d30c4383538a94861e69d8e125baacf3a25e0be8
Takuma Tsubo <ttsubo@c4r2s12.42tokyo.jp>
2024-10-28 18:01:22 +0900

    直前の符号だけ判定するように修正

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: ttsubo <ttsubo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/28 14:46:32 by ttsubo            #+#    #+#             */
-/*   Updated: 2024/10/28 15:19:43 by ttsubo           ###   ########.fr       */
+/*   Updated: 2024/10/28 18:00:49 by ttsubo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,19 +25,22 @@ int	ft_atoi(const char *str)
 {
 	int		i;
 	int		sign;
+	int		has_sign;
 	long	num;
 
 	i = 0;
 	sign = 1;
+	has_sign = 0;
 	num = 0;
 	while (_isspace(str[i]))
 		i++;
 	while (str[i] == '-' || str[i] == '+')
 	{
-		if (str[i] == '-')
-			sign *= -1;
+		has_sign = 1;
 		i++;
 	}
+	if (has_sign && str[i - 1] == '-')
+		sign = -1;
 	while (_isdigit(str[i]))
 	{
 		num = num * 10 + (str[i] - '0');

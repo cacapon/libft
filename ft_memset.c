74a9e2a1f312450ccc63a227739c52a027cc285e
Takuma Tsubo <ttsubo@c4r2s12.42tokyo.jp>
2024-10-28 11:58:28 +0900

    add ft_memset

--- ft_memset.c
+++ ft_memset.c
@@ -1,18 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_bzero.c                                         :+:      :+:    :+:   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ttsubo <ttsubo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/10/28 11:44:20 by ttsubo            #+#    #+#             */
-/*   Updated: 2024/10/28 11:57:03 by ttsubo           ###   ########.fr       */
+/*   Created: 2024/10/28 11:54:35 by ttsubo            #+#    #+#             */
+/*   Updated: 2024/10/28 11:58:17 by ttsubo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_bzero(void *b, size_t len)
+void	*ft_memset(void *b, int c, size_t len)
 {
-	ft_memset(b, '\0', len);
+	size_t	i;
+	char	*ptr;
+
+	i = 0;
+	ptr = (char *)b;
+	while (i < len)
+		ptr[i++] = (unsigned char)c;
+	return (b);
 }

5a8428ea9c0d4ea30c80d1a0d0a1e0e069767de5
Takuma Tsubo <ttsubo@c4r2s12.42tokyo.jp>
2024-10-28 14:36:04 +0900

    add ft_memchr

---
+++ ft_memchr.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ttsubo <ttsubo@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/28 14:28:42 by ttsubo            #+#    #+#             */
+/*   Updated: 2024/10/28 14:35:05 by ttsubo           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	size_t				i;
+	const unsigned char *us;
+
+	i = 0;
+	us = (const unsigned char *)s;
+	while (i < n)
+	{
+		if (us[i] == (unsigned char)c)
+			return ((void *)&us[i]);
+		i++;
+	}
+	return (NULL);
+}

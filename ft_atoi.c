74325571937178c14f9d7459caa0272076413e4f
Takuma Tsubo <ttsubo@c4r2s12.42tokyo.jp>
2024-10-28 15:21:31 +0900

    add ft_atoi

---
+++ ft_atoi.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ttsubo <ttsubo@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/28 14:46:32 by ttsubo            #+#    #+#             */
+/*   Updated: 2024/10/28 15:19:43 by ttsubo           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+static int	_isspace(int c)
+{
+	return (c == ' ' || c == '\f' || c == '\n' || c == '\r' || c == '\t'
+		|| c == '\v');
+}
+
+static int	_isdigit(int c)
+{
+	return ('0' <= c && c <= '9');
+}
+
+int	ft_atoi(const char *str)
+{
+	int		i;
+	int		sign;
+	long	num;
+
+	i = 0;
+	sign = 1;
+	num = 0;
+	while (_isspace(str[i]))
+		i++;
+	while (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			sign *= -1;
+		i++;
+	}
+	while (_isdigit(str[i]))
+	{
+		num = num * 10 + (str[i] - '0');
+		i++;
+	}
+	return (sign * (int)num);
+}

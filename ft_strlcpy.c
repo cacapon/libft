af6010eac1921eba2700d2046e9d1399f078fba3
Takuma Tsubo <ttsubo@c4r2s12.42tokyo.jp>
2024-10-29 13:46:49 +0900

    fix strlcpy

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: ttsubo <ttsubo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/28 12:30:54 by ttsubo            #+#    #+#             */
-/*   Updated: 2024/10/28 16:15:46 by ttsubo           ###   ########.fr       */
+/*   Updated: 2024/10/29 13:45:42 by ttsubo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,17 +22,37 @@ static int	_strlen(const char *str)
 	return (len);
 }
 
-size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
+static void	*_memcpy(void *dst, const void *src, size_t len)
 {
-	size_t	i;
+	unsigned char	*d;
+	unsigned char	*s;
+	size_t			i;
 
+	if (dst == NULL || src == NULL)
+		return (dst);
+	d = (unsigned char *)dst;
+	s = (unsigned char *)src;
 	i = 0;
-	while (src[i] && i < dstsize - 1)
+	while (i < len)
 	{
-		dst[i] = src[i];
+		d[i] = s[i];
 		i++;
 	}
-	if (dstsize > 0)
-		dst[i] = '\0';
-	return (_strlen(src));
+	return (dst);
+}
+
+size_t ft_strlcpy(char * dst, const char *src, size_t dstsize)
+{
+	size_t src_length = _strlen (src);
+	if ((src_length >= dstsize))
+  	{
+		if (dstsize > 0)
+		{
+			_memcpy(dst, src, dstsize);
+			dst[dstsize - 1] = '\0';
+		}
+    }
+	else
+		_memcpy (dst, src, src_length + 1);
+  	return src_length;
 }

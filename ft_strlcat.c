35bd65d628032149330f819cf1aa198b8ea547b5
Takuma Tsubo <ttsubo@c4r2s12.42tokyo.jp>
2024-10-29 16:01:46 +0900

    fix strlcat

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,13 +6,13 @@
 /*   By: ttsubo <ttsubo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/22 09:59:27 by ttsubo            #+#    #+#             */
-/*   Updated: 2024/10/28 16:15:33 by ttsubo           ###   ########.fr       */
+/*   Updated: 2024/10/29 16:01:26 by ttsubo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int	_strlen(const char *str)
+static size_t	_strlen(const char *str)
 {
 	int	len;
 
@@ -22,31 +22,30 @@ static int	_strlen(const char *str)
 	return (len);
 }
 
-size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
+static size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 {
-	const char		*odest;
-	const char		*osrc;
-	unsigned int	n;
-	unsigned int	dlen;
+	size_t	copied;
 
-	n = dstsize;
-	odest = dst;
-	osrc = src;
-	while (n-- != 0 && *dst)
-		dst++;
-	dlen = dst - odest;
-	n = dstsize - dlen;
-	if (n-- == 0)
-		return (dlen + _strlen(src));
-	while (*src)
+	copied = 0;
+	if (dstsize == 0)
+		return (_strlen(src));
+	while (copied < dstsize - 1 && *src)
 	{
-		if (n != 0)
-		{
-			*dst++ = *src;
-			n--;
-		}
-		src++;
+		*dst++ = *src++;
+		copied++;
 	}
 	*dst = '\0';
-	return (dlen + (src - osrc));
+	return (copied + _strlen(src));
+}
+
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
+{
+	size_t	l;
+
+	l = _strlen(dst);
+	if (l >= dstsize)
+		l = dstsize;
+	if (l == dstsize)
+		return (l + _strlen(src));
+	return (l + _strlcpy(dst + l, src, dstsize - l));
 }

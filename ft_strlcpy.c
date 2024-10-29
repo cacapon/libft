226e3f30cfdf200be93f00a1fb9bc3b55c70ee2c
Takuma Tsubo <ttsubo@c4r2s12.42tokyo.jp>
2024-10-29 14:40:40 +0900

    fix norm

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: ttsubo <ttsubo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/28 12:30:54 by ttsubo            #+#    #+#             */
-/*   Updated: 2024/10/29 13:45:42 by ttsubo           ###   ########.fr       */
+/*   Updated: 2024/10/29 14:40:20 by ttsubo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,18 +41,20 @@ static void	*_memcpy(void *dst, const void *src, size_t len)
 	return (dst);
 }
 
-size_t ft_strlcpy(char * dst, const char *src, size_t dstsize)
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 {
-	size_t src_length = _strlen (src);
+	size_t	src_length;
+
+	src_length = _strlen(src);
 	if ((src_length >= dstsize))
-  	{
+	{
 		if (dstsize > 0)
 		{
 			_memcpy(dst, src, dstsize);
 			dst[dstsize - 1] = '\0';
 		}
-    }
+	}
 	else
-		_memcpy (dst, src, src_length + 1);
-  	return src_length;
+		_memcpy(dst, src, src_length + 1);
+	return (src_length);
 }

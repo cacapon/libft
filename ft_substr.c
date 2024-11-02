a6b5d0c6c64370a1aeb3ca3b14ee431fa16180fa
Takuma Tsubo <ttsubo@c4r2s12.42tokyo.jp>
2024-11-02 15:10:34 +0900

    fix substr (norm)

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: ttsubo <ttsubo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/29 14:45:51 by ttsubo            #+#    #+#             */
-/*   Updated: 2024/11/02 13:49:10 by ttsubo           ###   ########.fr       */
+/*   Updated: 2024/11/02 15:09:38 by ttsubo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 		return (NULL);
 	s_len = ft_strlen(s);
 	if (start >= s_len)
-		return ft_strdup("");
+		return (ft_strdup(""));
 	if (start + len >= s_len)
 		len = s_len - start;
 	sub = malloc(len + 1);

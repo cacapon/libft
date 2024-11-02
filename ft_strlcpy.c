234e3963341913cbed68412f9eea3a6a621d00de
Takuma Tsubo <ttsubo@c4r2s12.42tokyo.jp>
2024-11-02 13:54:49 +0900

    fix strlcpy

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: ttsubo <ttsubo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/28 12:30:54 by ttsubo            #+#    #+#             */
-/*   Updated: 2024/11/02 13:49:13 by ttsubo           ###   ########.fr       */
+/*   Updated: 2024/11/02 13:54:15 by ttsubo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,15 @@
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 {
 	size_t	copied;
-	size_t	src_len;
 
 	copied = 0;
-	src_len = ft_strlen(src);
 	if (dstsize == 0)
-		return (src_len);
+		return (ft_strlen(src));
 	while (copied < dstsize - 1 && *src)
 	{
 		*dst++ = *src++;
 		copied++;
 	}
 	*dst = '\0';
-	return (copied + src_len);
+	return (copied + ft_strlen(src));
 }

5202ea7bf7402a08e938dddc288390d4cfb631db
Cacapon Project <cacapon.project@gmail.com>
2024-11-04 18:58:39 +0900

    fix ft_memmove and ft_memcopy

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: ttsubo <ttsubo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/28 13:26:29 by ttsubo            #+#    #+#             */
-/*   Updated: 2024/11/04 18:18:29 by ttsubo           ###   ########.fr       */
+/*   Updated: 2024/11/04 18:57:58 by ttsubo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,11 @@
 // This was created with the musl no conditions as a reference.
 // ATTENTION:
 // 	memcpy(NULL,NULL,3) will cause a segfault, unlike the standard mac library.
-// 	Depending on the unit test, it may cause a crash error, but we will not respond to it due to undefined behavior.
+// 	Depending on the unit test, it may cause a crash error,
+//	but we will not respond to it due to undefined behavior.
 void	*ft_memcpy(void *dst, const void *src, size_t len)
 {
-	unsigned char	*d;
+	unsigned char		*d;
 	const unsigned char	*s;
 
 	d = (unsigned char *)dst;
--- ft_memmove.c
+++ ft_memmove.c
@@ -6,33 +6,30 @@
 /*   By: ttsubo <ttsubo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/28 13:44:03 by ttsubo            #+#    #+#             */
-/*   Updated: 2024/10/28 14:25:02 by ttsubo           ###   ########.fr       */
+/*   Updated: 2024/11/04 18:57:10 by ttsubo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+// This was created with reference to the code
+// excluding the musl GNUC optimization.
 void	*ft_memmove(void *dst, const void *src, size_t len)
 {
 	unsigned char		*d;
 	const unsigned char	*s;
-	size_t				i;
 
-	if (dst == NULL || src == NULL)
-		return (NULL);
 	d = (unsigned char *)dst;
 	s = (const unsigned char *)src;
-	if (d < s || d >= s + len)
-	{
-		i = -1;
-		while (++i < len)
-			d[i] = s[i];
-	}
+	if (s == d)
+		return (d);
+	if ((uintptr_t)s - (uintptr_t)d - len <= -2 * len)
+		return (ft_memcpy(d, s, len));
+	if (d < s)
+		while (len--)
+			*d++ = *s++;
 	else
-	{
-		i = len + 1;
-		while (--i > 0)
-			d[i - 1] = s[i - 1];
-	}
+		while (len--)
+			d[len] = s[len];
 	return (dst);
 }

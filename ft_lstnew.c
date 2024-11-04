369f2562a16593ab99819d424ce5d1e3be204e09
Cacapon Project <cacapon.project@gmail.com>
2024-11-04 21:55:09 +0900

    add ft_lstnew

--- ft_lstnew.c
+++ ft_lstnew.c
@@ -6,7 +6,7 @@
 /*   By: ttsubo <ttsubo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/04 21:05:43 by ttsubo            #+#    #+#             */
-/*   Updated: 2024/11/04 21:12:53 by ttsubo           ###   ########.fr       */
+/*   Updated: 2024/11/04 21:54:50 by ttsubo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,12 @@
 
 t_list	*ft_lstnew(void *content)
 {
-	(void) content;
-	return (NULL);
+	t_list	*node;
+
+	node = (t_list *)malloc(sizeof(t_list*));
+	if (node == NULL)
+		return (NULL);
+	node->content = content;
+	node->next = NULL;
+	return (node);
 }

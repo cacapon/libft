3996341a0ad789c84b1b155904d86a852a261414
Takuma Tsubo <ttsubo@c4r2s12.42tokyo.jp>
2024-11-05 19:00:22 +0900

    fix size_t対策

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: ttsubo <ttsubo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/28 12:12:21 by ttsubo            #+#    #+#             */
-/*   Updated: 2024/11/05 17:27:38 by ttsubo           ###   ########.fr       */
+/*   Updated: 2024/11/05 18:59:43 by ttsubo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,10 @@ char	*ft_strrchr(const char *s, int c)
 	size_t	i;
 
 	i = ft_strlen(s);
-	while (i >= 0)
-	{
+	while (i-- > 0)
 		if (s[i] == (char)c)
 			return ((char *)&s[i]);
-		i--;
-	}
+	if (s[i] == (char)c)
+		return ((char *)&s[i]);
 	return (NULL);
 }

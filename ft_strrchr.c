7245e91dfb28590e96457f321c254e1b054e431c
cacapon <takuma.tsubo@amazingengine.co.jp>
2024-11-05 19:23:37 +0900

    fix ft_strrchr (msul base)

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,21 +6,14 @@
 /*   By: ttsubo <ttsubo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/28 12:12:21 by ttsubo            #+#    #+#             */
-/*   Updated: 2024/11/05 18:59:43 by ttsubo           ###   ########.fr       */
+/*   Updated: 2024/11/05 19:23:20 by ttsubo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+// Created with reference to musl.
 char	*ft_strrchr(const char *s, int c)
 {
-	size_t	i;
-
-	i = ft_strlen(s);
-	while (i-- > 0)
-		if (s[i] == (char)c)
-			return ((char *)&s[i]);
-	if (s[i] == (char)c)
-		return ((char *)&s[i]);
-	return (NULL);
+	return (ft_memchr(s, c, ft_strlen(s) + 1));
 }

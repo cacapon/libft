ec21812cb9c24cfc8cf05ca8f43cfb43bb63a147
cacapon <takuma.tsubo@amazingengine.co.jp>
2024-11-05 19:28:31 +0900

    fix ft_strrchr (msul base)

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: ttsubo <ttsubo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/28 12:12:21 by ttsubo            #+#    #+#             */
-/*   Updated: 2024/11/05 19:23:20 by ttsubo           ###   ########.fr       */
+/*   Updated: 2024/11/05 19:28:18 by ttsubo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,12 @@
 // Created with reference to musl.
 char	*ft_strrchr(const char *s, int c)
 {
-	return (ft_memchr(s, c, ft_strlen(s) + 1));
+	size_t	n;
+
+	n = ft_strlen(s) + 1;
+	c = (unsigned char)c;
+	while (n--)
+		if (s[n] == c)
+			return (char *)(s + n);
+	return (0);
 }

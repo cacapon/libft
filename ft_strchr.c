5734f3507586cfe9fe4ceb8096e1d265bbf37338
cacapon <takuma.tsubo@amazingengine.co.jp>
2024-11-06 19:59:11 +0900

    fix strchr

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,32 +6,46 @@
 /*   By: ttsubo <ttsubo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/28 12:12:21 by ttsubo            #+#    #+#             */
-/*   Updated: 2024/11/06 11:07:19 by ttsubo           ###   ########.fr       */
+/*   Updated: 2024/11/06 19:55:26 by ttsubo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 /*
 	DESCRIPTION:
-		SUMMARY:
+		Searches for the passed string with c and returns its location.
 		ARGS:
+			s:	string to be searched
+			c:	Characters to search for (passed as ASCII digits)
 	IN:
+		s:	NULL or allocated memory.
+		c:	0 <= c <= UCHAR_MAX
 	OUT:
+		normal:
+			found c:
+				return c address.
+			not found c:
+				return NULL
+		s is NULL:
+			return NULL
+		c<0:
+			not found -> return NULL
+		127<c:
+			not found -> return NULL
 */
 
 #include "libft.h"
 
 char	*ft_strchr(const char *s, int c)
 {
-	size_t	i;
-
-	i = 0;
-	while (s[i])
+	if (!s || c < 0 || 127 < c)
+		return (NULL);
+	while (*s)
 	{
-		if (s[i] == (char)c)
-			return ((char *)&s[i]);
-		i++;
+		if (*s == (char)c)
+			return ((char *)s);
+		s++;
 	}
 	if (c == '\0')
-		return ((char *)&s[i]);
+		return ((char *)s);
 	return (NULL);
 }

8e03b6211f00778c9a18b41f8c21dbbb66a627ec
cacapon <takuma.tsubo@amazingengine.co.jp>
2024-11-06 10:26:01 +0900

    Add comments

--- ft_isalnum.c
+++ ft_isalnum.c
@@ -6,9 +6,23 @@
 /*   By: ttsubo <ttsubo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/26 18:42:55 by ttsubo            #+#    #+#             */
-/*   Updated: 2024/11/02 12:57:55 by ttsubo           ###   ########.fr       */
+/*   Updated: 2024/11/06 10:25:47 by ttsubo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+/*
+	DESCRIPTION:
+		SUMMARY:
+			Find out if it is alphabetical or numeric.
+		ARGS:
+			c:	ASCII code number.
+	IN:
+		c:
+			INT_MIN <= c <= INT_MAX
+	OUT:
+		c = [A-Za-z] or [0-9]	-> 1
+		else					-> 0
+*/
 
 #include "libft.h"
 

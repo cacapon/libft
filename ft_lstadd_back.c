facaab5c48bc984dd9aa2c34f72b3fa17f81f75d
cacapon <takuma.tsubo@amazingengine.co.jp>
2024-11-06 15:59:09 +0900

    add docments

--- ft_lstadd_back.c
+++ ft_lstadd_back.c
@@ -6,16 +6,27 @@
 /*   By: ttsubo <ttsubo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/04 22:29:50 by ttsubo            #+#    #+#             */
-/*   Updated: 2024/11/06 11:05:40 by ttsubo           ###   ########.fr       */
+/*   Updated: 2024/11/06 15:43:50 by ttsubo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 /*
 	DESCRIPTION:
-		SUMMARY:
+		Add node 'new' to the end of the list.
 		ARGS:
+			lst: pointer address to the first link in the list
+			new: pointer address to the node to be added to the list
 	IN:
+		lst:	NULL or Allocated memory
+		new:	NULL or Allocated memory	
 	OUT:
+		normal:
+			No return value
+			Add new to the end of lst.
+		lst == NULL:
+			make lst with new at the top.
+		new:
+			Adds null to subsequent but practically meaningless.
 */
 
 #include "libft.h"

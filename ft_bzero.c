cb8b3a0a5c09c6252c13a3e9595ccef92f5cea4c
cacapon <takuma.tsubo@amazingengine.co.jp>
2024-11-06 12:50:15 +0900

    fix ft_bzero.c and ft_memset.c

--- ft_bzero.c
+++ ft_bzero.c
@@ -6,21 +6,33 @@
 /*   By: ttsubo <ttsubo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/28 11:44:20 by ttsubo            #+#    #+#             */
-/*   Updated: 2024/11/06 11:02:12 by ttsubo           ###   ########.fr       */
+/*   Updated: 2024/11/06 12:49:18 by ttsubo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 /*
 	DESCRIPTION:
-		SUMMARY:
+		Set 0 to the specified length for the given memory area.
 		ARGS:
+			b:		Memory address to be set to 0
+			len:	Memory length to set 0
 	IN:
+		b:	NULL or allocated memory
+		len: 0 <= len <= SIZE_T_MAX
 	OUT:
+		normal:
+			No return value.
+			b replaces the content with 0 for the length of len
+			If len=0, nothing is done.
+		b is NULL:
+			Nothing to do.
 */
 
 #include "libft.h"
 
 void	ft_bzero(void *b, size_t len)
 {
-	ft_memset(b, '\0', len);
+	if (!b)
+		return ;
+	ft_memset(b, 0, len);
 }
--- ft_memset.c
+++ ft_memset.c
@@ -6,16 +6,26 @@
 /*   By: ttsubo <ttsubo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/28 11:54:35 by ttsubo            #+#    #+#             */
-/*   Updated: 2024/11/06 11:06:58 by ttsubo           ###   ########.fr       */
+/*   Updated: 2024/11/06 12:47:51 by ttsubo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 /*
 	DESCRIPTION:
-		SUMMARY:
+		Write c to len byte string b.
 		ARGS:
+			b:		Memory area to write
+			c:		character (internally converted to unsigned char)
+			len:	Length of writing c to memory
 	IN:
+		b:		NULL or allocated memory space.
+		c:		0 <= c <= UCHAR_MAX
+		len:	0 <= len <= SIZE_T_MAX
 	OUT:
+		normal:		return b (c already written)
+		b is NULL:	return b (Nothing to do)
+		c < 0:		return b (Nothing to do)
+		255 < c:	return b (Nothing to do)
 */
 
 #include "libft.h"
@@ -25,8 +35,10 @@ void	*ft_memset(void *b, int c, size_t len)
 	size_t	i;
 	char	*ptr;
 
+	if (!b || c < 0 || UCHAR_MAX < c)
+		return (b);
 	i = 0;
-	ptr = (char *)b;
+	ptr = (unsigned char *)b;
 	while (i < len)
 		ptr[i++] = (unsigned char)c;
 	return (b);

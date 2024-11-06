21beb72e3bd5692f5f942a0a407834911edd43a7
cacapon <takuma.tsubo@amazingengine.co.jp>
2024-11-06 20:32:52 +0900

    add docs.

--- ft_strlen.c
+++ ft_strlen.c
@@ -6,16 +6,22 @@
 /*   By: ttsubo <ttsubo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/26 18:57:35 by ttsubo            #+#    #+#             */
-/*   Updated: 2024/11/06 11:07:46 by ttsubo           ###   ########.fr       */
+/*   Updated: 2024/11/06 20:31:22 by ttsubo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 /*
 	DESCRIPTION:
-		SUMMARY:
+		Returns the length of a string
 		ARGS:
+			s:	string	
 	IN:
+		s:	NULL or allocated memory.
 	OUT:
+		normal:
+			return lentgh of a string
+		s is NULL
+			return 0;
 */
 
 #include "libft.h"
@@ -24,6 +30,8 @@ size_t	ft_strlen(const char *s)
 {
 	size_t	len;
 
+	if (!s)
+		return (0);
 	len = 0;
 	while (s[len])
 		len++;

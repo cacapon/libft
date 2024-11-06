f4e94756a628d0e8aaae23ceabd7b33b643cd988
cacapon <takuma.tsubo@amazingengine.co.jp>
2024-11-06 19:06:33 +0900

    fix test

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,16 +6,31 @@
 /*   By: ttsubo <ttsubo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/28 14:36:21 by ttsubo            #+#    #+#             */
-/*   Updated: 2024/11/06 11:06:49 by ttsubo           ###   ########.fr       */
+/*   Updated: 2024/11/06 19:04:13 by ttsubo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 /*
 	DESCRIPTION:
-		SUMMARY:
+		Compare byte strings
 		ARGS:
+			s1:	byte strings 1
+			s2: byte strings 2
+			n:	Length to compare.
 	IN:
+		s1:	NULL or allocated memory.
+		s2:	NULL or allocated memory.
+		n:	0 <= n <= SIZET_MAX
 	OUT:
+		normal:
+			s1 and s2 are the same:
+				return 0
+			Differential:
+				Return the difference of mismatched byte sequences
+		s1 is NULL or s2 is NULL:
+			Operation is not guaranteed due to undefined operation.
+		s1.length < n or s2.length < n:
+			Operation is not guaranteed due to undefined operation.
 */
 
 #include "libft.h"

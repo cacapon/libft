6b9b3a812ddb1ffa57ebca248965c2f9d0c22ea3
cacapon <takuma.tsubo@amazingengine.co.jp>
2024-11-06 20:03:40 +0900

    add docs

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,16 +6,25 @@
 /*   By: ttsubo <ttsubo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/28 12:00:38 by ttsubo            #+#    #+#             */
-/*   Updated: 2024/11/06 11:07:29 by ttsubo           ###   ########.fr       */
+/*   Updated: 2024/11/06 20:03:21 by ttsubo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 /*
 	DESCRIPTION:
-		SUMMARY:
+		Copy s1 to the newly allocated area
 		ARGS:
+			s1:	String to copy
 	IN:
+		s:	NULL or allocated memory.
 	OUT:
+		normal:
+			Returns the top address 
+			of the memory from which the newly allocated s1 was copied
+		malloc failed:
+			return NULL
+		s1 is NULL
+			return NULL
 */
 
 #include "libft.h"
@@ -25,6 +34,8 @@ char	*ft_strdup(const char *s1)
 	size_t	l;
 	char	*d;
 
+	if (!s1)
+		return (NULL);
 	l = (size_t)ft_strlen((char *)s1);
 	d = malloc(l + 1);
 	if (!d)

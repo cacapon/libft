9a1fe0fcae4a7899adabcf122c7fabf9c16ed5db
Cacapon Project <cacapon.project@gmail.com>
2024-11-09 12:14:25 +0900

    add docs

--- ft_tolower.c
+++ ft_tolower.c
@@ -6,16 +6,20 @@
 /*   By: ttsubo <ttsubo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/28 11:38:48 by ttsubo            #+#    #+#             */
-/*   Updated: 2024/11/06 11:08:11 by ttsubo           ###   ########.fr       */
+/*   Updated: 2024/11/09 12:13:14 by ttsubo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 /*
 	DESCRIPTION:
-		SUMMARY:
+		Change to lower case
 		ARGS:
+			c:	ASCII code number.
 	IN:
+			INT_MIN <= c <= INT_MAX
 	OUT:
+		c = [A-Z]	-> return c + 32
+		else		-> return c
 */
 
 #include "libft.h"

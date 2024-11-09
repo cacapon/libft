229066812578b5c6479345758294b67ba9671d51
Takuma Tsubo <ttsubo@c4r2s12.42tokyo.jp>
2024-11-09 17:31:38 +0900

    fix lstnew(malloc size)

--- ft_lstnew.c
+++ ft_lstnew.c
@@ -6,7 +6,7 @@
 /*   By: ttsubo <ttsubo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/04 21:05:43 by ttsubo            #+#    #+#             */
-/*   Updated: 2024/11/06 18:22:13 by ttsubo           ###   ########.fr       */
+/*   Updated: 2024/11/09 17:30:37 by ttsubo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,7 +31,7 @@ t_list	*ft_lstnew(void *content)
 {
 	t_list	*node;
 
-	node = (t_list *)malloc(sizeof(t_list *));
+	node = (t_list *)malloc(sizeof(t_list));
 	if (node == NULL)
 		return (NULL);
 	node->content = content;

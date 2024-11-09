dc83e737278a7a325d6da70d06e4efab47c598a5
Cacapon Project <cacapon.project@gmail.com>
2024-11-09 12:09:43 +0900

    add docs

--- ft_substr.c
+++ ft_substr.c
@@ -6,16 +6,31 @@
 /*   By: ttsubo <ttsubo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/29 14:45:51 by ttsubo            #+#    #+#             */
-/*   Updated: 2024/11/06 11:08:09 by ttsubo           ###   ########.fr       */
+/*   Updated: 2024/11/09 12:08:40 by ttsubo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 /*
 	DESCRIPTION:
-		SUMMARY:
+		Extract a substring from a character string s
 		ARGS:
+			s:		string
+			start:	Starting position for extraction
+			len:	Extractable size
 	IN:
+		s1:		NULL or allocated memory.
+		start:	0 <= start <= len -1
+		len:	0 <= len   <= SIZE_T_MAX
 	OUT:
+		normal:
+			return substring (of length s or len)
+		Failed to allocate memory for partial string:
+			return NULL
+		undifined:
+			s is NULL
+				return NULL
+			start is greater than the actual length of s
+				return ft_strdup("")
 */
 
 #include "libft.h"

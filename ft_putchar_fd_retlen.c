eb4648d5737b41a59977e434de2ea9e68ce89b9c
Takuma Tsubo <ttsubo@c4r2s12.42tokyo.jp>
2024-11-20 12:42:11 +0900

    add retlen function

---
+++ ft_putchar_fd_retlen.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putchar_fd_retlen.c                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ttsubo <ttsubo@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/01 21:08:33 by ttsubo            #+#    #+#             */
+/*   Updated: 2024/11/20 12:39:33 by ttsubo           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+/**
+ * @brief Writes the letter c to the file descriptor
+ *
+ * @param c			: character
+ * @param fd 		: file descriptor
+ * @return ssize_t	: return Return the number written, or -1.
+ */
+ssize_t	ft_putchar_fd_retlen(char c, int fd)
+{
+	return (write(fd, &c, 1));
+}
---
+++ ft_putstr_fd_retlen.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr_fd_retlen.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ttsubo <ttsubo@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/01 21:08:33 by ttsubo            #+#    #+#             */
+/*   Updated: 2024/11/20 12:41:39 by ttsubo           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+/**
+ * @brief Writes strings to file descriptors 
+ * 
+ * @param s 		: string to write
+ * @param fd 		: file descriptor
+ * @return ssize_t	: Return the number written, or -1.
+ */
+ssize_t	ft_putstr_fd(char *s, int fd)
+{
+	if (s == NULL)
+		return (0);
+	return (write(fd, s, ft_strlen(s)));
+}

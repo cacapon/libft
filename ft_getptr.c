495adbc7ed4d48ad3a5bf78ad246cb4e927c33ec
Takuma Tsubo <ttsubo@c4r2s12.42tokyo.jp>
2024-12-03 14:56:05 +0900

    余分な0出力をしないように修正

--- ft_getptr.c
+++ ft_getptr.c
@@ -6,7 +6,7 @@
 /*   By: ttsubo <ttsubo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/01 21:08:33 by ttsubo            #+#    #+#             */
-/*   Updated: 2024/11/23 16:36:46 by ttsubo           ###   ########.fr       */
+/*   Updated: 2024/12/03 14:54:47 by ttsubo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,12 +32,15 @@ void	ft_getptr(char *dst, void *ptr, size_t dstsize)
 	address = (uintptr_t)ptr;
 	dst[0] = '0';
 	dst[1] = 'x';
-	i = (sizeof(address) * 2) - 1;
+	i = 15;
 	while (i >= 0)
 	{
 		dst[2 + i] = "0123456789abcdef"[address & 0xF];
 		address >>= 4;
 		i--;
 	}
-	dst[2 + (sizeof(address) * 2)] = '\0';
+	i = 2;
+	while (dst[i] == '0' && i < 16)
+		i++;
+	ft_strlcpy(dst + 2, dst + i, dstsize - 2);
 }

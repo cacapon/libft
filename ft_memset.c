c1ff51c79f0609a1b531ab62ae6fc39cda1f6ae3
Cacapon Project <cacapon.project@gmail.com>
2024-12-23 19:38:27 +0900

    コメント変更&ガード式を一部削除

--- ft_memset.c
+++ ft_memset.c
@@ -6,36 +6,30 @@
 /*   By: ttsubo <ttsubo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/28 11:54:35 by ttsubo            #+#    #+#             */
-/*   Updated: 2024/11/06 14:36:03 by ttsubo           ###   ########.fr       */
+/*   Updated: 2024/12/23 19:38:03 by ttsubo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-/*
-	DESCRIPTION:
-		Write c to len byte string b.
-		ARGS:
-			b:		Memory area to write
-			c:		character (internally converted to unsigned char)
-			len:	Length of writing c to memory
-	IN:
-		b:		NULL or allocated memory space.
-		c:		0 <= c <= UCHAR_MAX
-		len:	0 <= len <= SIZE_T_MAX
-	OUT:
-		normal:		return b (c already written)
-		b is NULL:	return b (Nothing to do)
-		c < 0:		return b (Nothing to do)
-		255 < c:	return b (Nothing to do)
-*/
-
 #include "libft.h"
 
+/**
+ * @brief メモリ領域bを文字cで穴埋めします
+ *
+ * @param [out]	b	: メモリ領域
+ * @param [in]	c	: 穴埋めする文字
+ * @param [in]	len	: 穴埋めする長さ
+ * @retval void*	: 穴埋めしたメモリ領域
+ * @retval NULL		: bがNULLの場合
+ * @note : 
+ * 内部でuchar型にキャストするので、
+ * 0-255の範囲外の数字は上位ビットが無視された値で使われます。
+ */
 void	*ft_memset(void *b, int c, size_t len)
 {
 	size_t			i;
 	unsigned char	*ptr;
 
-	if (!b || c < 0 || UCHAR_MAX < c)
+	if (!b)
 		return (b);
 	i = 0;
 	ptr = (unsigned char *)b;
